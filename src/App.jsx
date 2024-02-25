d0731c78acb80f5067e7cee183d481c3dffd605b
Abhi <meabhishek1512@gmail.com>
2024-02-25 13:58:11 +0530

    All the files added except readme.md

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react"
+import "./App.css"
+import SingleCard from "./components/SingleCard"
+
+const cardImages = [
+  { src: "/img/luffy.jpg", matched: false },
+  { src: "/img/zoro.jpg", matched: false },
+  { src: "/img/robin.jpg", matched: false },
+  { src: "/img/nami.jpg", matched: false },
+  { src: "/img/sanji.jpg", matched: false },
+  { src: "/img/usopp.jpg", matched: false },
+]
+
+const App = () => {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  // shuffle card
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices and increase turns
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns((prevTurns) => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // start a  new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>
+        <span>ONE PIECE</span> Memory Game
+      </h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  )
+}
+
+export default App
